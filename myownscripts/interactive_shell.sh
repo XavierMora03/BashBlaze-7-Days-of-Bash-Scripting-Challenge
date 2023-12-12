86609eb13271ad313619d7644d35ccdfae69b0a0
XavierMora03 <diego.mora5781@alumnos.udg.mx>
2023-12-11 23:58:10 -0600

    my first bash scripts

---
+++ myownscripts/interactive_shell.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo What is your name?
+read FIRST
+echo What is your last name?
+read LAST
+
+echo ok you name is $FIRST $LAST
---
+++ myownscripts/shelltest.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo Hello Wolrd!
+
+
+FIRST=Diego
+LAST=Mora
+
+echo My name is $FIRST $LAST
